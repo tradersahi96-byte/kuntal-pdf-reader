c8f5fbea4e3eef32ce46fb67e7a3c21d845d134e
tradersahi96-byte <tradersahi96@gmail.com>
2026-09-01 17:27:03 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,2 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
-module.exports = getDefaultConfig(__dirname);
+
+const config = getDefaultConfig(__dirname);
+
+// Enable inline asset resolution and support Base64/PDF rendering
+config.resolver.assetExts.push('pdf', 'png', 'jpg', 'jpeg');
+
+module.exports = config;
